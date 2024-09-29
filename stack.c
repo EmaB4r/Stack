28ff5a063f4c4e6bfe26d5e6868baa8a7047b195
EmaB4r <emanuelebar323@gmail.com>
2024-09-29 11:06:16 +0200

    code refactor; added stack_peek and wrappers (peek_top and peek_bottom)

--- stack.c
+++ stack.c
@@ -2,78 +2,60 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-stack_t global_stack = NULL;
-
-
-void stack_push(void * item, size_t item_size){
-    if(global_stack==NULL){
-        global_stack=sstack_init(100);
-    }
-    sstack_push(global_stack, item, item_size);
-}
-
-void* stack_pop(){
-    if(global_stack==NULL){
-        global_stack=sstack_init(100);
-        return NULL;
-    }
-    return sstack_pop(global_stack);
-}
-
-void stack_print(char* str){
-    if(global_stack!=0) sstack_print(global_stack, str);
-}
-
-void stack_free(){
-    if(global_stack!=NULL) sstack_free(global_stack);
-}
-
+int shithappens=-1;
 
 //stack_init
-stack_t sstack_init(unsigned size){
-    if(size<100) size = 100;
-    stack_t s = malloc(sizeof(*s));
-    s->used_space=0;
-    s->stack_size=size;
-    s->data=malloc(sizeof(uint8_t)*size);
+stack_t stack_init(unsigned size, unsigned item_size){
+    //if(size<100) size = 100;
+    stack_t s;
+    s.used_space=0;
+    s.stack_size=size;
+    s.item_size=item_size;
+    s.data=malloc(size*item_size);
     return s;
 }
 
-void sstack_push(stack_t stack, void* item, size_t item_size){
-    if(stack->used_space+item_size + PADDING < stack->stack_size){
+void stack_push(stack_t* stack, void* item){
+    if(stack->used_space+1 >= stack->stack_size){
         stack->stack_size*=1.5;
         stack->data=realloc(stack->data, sizeof(uint8_t)*stack->stack_size);
     } 
-    BYTECOPY(&stack->data[stack->used_space], item, item_size);
-    stack->used_space+=item_size;
-    *(size_t*)&stack->data[stack->used_space]=item_size;
-    stack->used_space+=PADDING;
+    memcpy(&stack->data[stack->used_space*stack->item_size], item, stack->item_size);
+    stack->used_space++;
 }
-void * sstack_pop(stack_t stack){
-    stack->used_space-=PADDING;
-    size_t item_size = *(size_t*)&stack->data[stack->used_space];
-    stack->used_space-=item_size;
-    return &stack->data[stack->used_space];
+void * stack_pop(stack_t* stack){
+    if(stack->used_space==0) return &shithappens;
+    stack->used_space--;
+    return &stack->data[stack->used_space*stack->item_size];
 }
 
-void sstack_print(stack_t stack, char* str){
-    int SP=stack->used_space-PADDING;
-    size_t objsize;
+void * stack_peek(stack_t* stack, unsigned index){
+    if(index >= stack->used_space) return &shithappens;
+    return &stack->data[stack->item_size*(stack->used_space-index-1)];
+}
+
+void * stack_peek_top(stack_t* stack){
+    return stack_peek(stack, 0);
+}
+
+void * stack_peek_bottom(stack_t* stack){
+    return stack_peek(stack, stack->used_space-1);
+}
+
+void stack_print(stack_t* stack, char* fmt){
+    int SP=stack->used_space-1;
     printf("--TOP--\n");
-    while(SP>0){
-        objsize = *(size_t*)&stack->data[SP];
-        SP-=objsize;
-        printf(str, *(size_t*)&stack->data[SP]);
-        SP-=PADDING;
+    while(SP>=0){
+        printf(fmt, (int)stack->data[SP*stack->item_size]);
+        SP--;
     }
     printf("--BOTTOM--\n");
 }
 
-void sstack_free(stack_t  stack){
+void stack_free(stack_t* stack){
     if (stack!=NULL){
         if(stack->data!=NULL){
             free(stack->data);
         }
-        free(stack);
     }
 }
--- stack.h
+++ stack.h
@@ -2,65 +2,41 @@
 #define STACK_H
 #include <stdint.h>
 #include <stddef.h>
+#include <string.h>
 
 //used to store on the stack the size of the item below
 #define PADDING sizeof(size_t)
-
-//given two pointers (supposed same memory size allocated) and the allocated mem size
-//copies bytes values from SRCPTR to DESTPTR
-#define BYTECOPY(DESTPTR, SRCPTR, SRCSIZE)        \
-    do{                                     \
-        for (int I =0; I<SRCSIZE; I++){     \
-            ((char*)DESTPTR)[I]=((char*)SRCPTR)[I];           \
-        }                                   \
-    }while(0)
-
-
 
 typedef struct stack_s{
     uint8_t* data;
     unsigned stack_size;
     unsigned used_space;
-}*stack_t;
-
-//saves <item_size> bytes starting from pointer item onto the GLOBAL stack  
-void stack_push(void * item, size_t item_size);
-
-//pops first item from the GLOBAL stack and returns a pointer to it
-// #CAST NEEDED# like type var = *(type*)stack_pop(); 
-void* stack_pop();
-
-void stack_print(char* str);
-
-//frees GLOBAL stack obj
-void stack_free();
+    unsigned item_size;
+}stack_t;
 
 //initializes a stack obj
-stack_t sstack_init(unsigned size);
+stack_t stack_init(unsigned size, unsigned item_size);
 
 //saves <item_size> bytes starting from pointer item onto the given stack  
-void sstack_push(stack_t stack, void* item, size_t item_size);
+void stack_push(stack_t* stack, void* item);
 
 //pops first item from the stack and returns a pointer to it
 // #CAST NEEDED# like type var = *(type*)stack_pop(); 
-void * sstack_pop(stack_t stack);
+void * stack_pop(stack_t* stack);
 
-void sstack_print(stack_t stack, char* str);
+void * stack_peek(stack_t* stack, unsigned index);
+
+void * stack_peek_top(stack_t* stack);
+
+void * stack_peek_bottom(stack_t* stack);
+
+void stack_print(stack_t* stack, char* fmt);
 
 //frees given stack
-void sstack_free(stack_t  stack);
-
-
-//pushes an item variable onto the GLOBAL stack
-#define STACK_PUSH(ITEM) stack_push(&ITEM, sizeof(ITEM))
-
-#define STACK_PUSH_NUM(ITEM_TYPE, ITEM) do{ITEM_TYPE VAR = ITEM; STACK_PUSH(VAR);}while(0)
+void stack_free(stack_t*  stack);
 
 //pushes an item variable onto the given stack
 #define SSTACK_PUSH(STACK,ITEM) sstack_push(STACK, &ITEM, sizeof(ITEM))
-
-//pops a value from the GLOBAL stack and casts it to TYPE
-#define STACK_POP(TYPE) (*(TYPE*)stack_pop())
 
 //pops a value from the given stack and casts it to TYPE
 #define SSTACK_POP(STACK, TYPE) (*(TYPE*)sstack_pop(STACK))
